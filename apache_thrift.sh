3b055eddfb582988333a8e386e50624de41468fd
Emily Curtin <emily@framebit.org>
2015-03-12 16:06:47 -0400

    added apache thrift instructions

---
+++ apache_thrift.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+#taken directly from apache thrifft ubuntu install site
+sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
+sudo apt-get install openjdk-7-jdk
