719c26db9ea38731d366bc2a3d3ac0b339fa7fd3
Emily Curtin <emily@framebit.org>
2015-03-12 16:07:25 -0400

    my ongoing list of packages to get a new linux system setup

---
+++ linux_setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install \
+make \
+texlive-latex3 \
+openjdk-7-jdk \
+python \
+gcc \
+g++ \
+pidgin \
+chromium-browser \
+nano \
+vim \
+git
+
+# sublime text: download .deb from their website and build it
